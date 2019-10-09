49d12ef7fd4bc4f9ce5ee2ab036858d079eb70f0
saras <sash19ad@student.cbs.com>
2019-10-09 16:42:52 +0200

    Hej121 fra henri

---
+++ trainingmodules.js
@@ -0,0 +1,15 @@
+class Trainingmodules {
+ constructor(fitnessCenter, date, time, trainingType)   {
+
+this.fitnessCenter = fitnessCenter;
+this.date = date;
+this.time = time;
+this.trainingTyper = trainingType;
+
+ }
+
+}
+
+var userOne = new Trainingmodules('Fitness World', '09/10/2019', '16:29', 'Styrketræning');
+
+console.log(userOne);
