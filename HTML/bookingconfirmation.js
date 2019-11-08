869c1c770ebfa112e24dcecd23798657d1448325
oliverdahl <olch19ab@student.cbs.dk>
2019-11-08 14:04:37 +0100

    Saras ændringer med local storage implementeret i vores bookingsystem.

--- HTML/bookingconfirmation.js
+++ HTML/bookingconfirmation.js
@@ -38,5 +38,34 @@ function getValueFromList() {
 
 
 
+this.trainingTypeInput = document.getElementById("trainingType");
+this.timeDateMonthInput = document.getElementById("timeDateMonth");
+this.timeForTrainingInput = document.getElementById("timeForTraining");
+this.personalTrainerOfTheDayInput = document.getElementById("personalTrainerOfTheDay");
+
+function saveBooking() {
+
+    localStorage.setItem("trainingType", trainingTypeInput.value);
+    localStorage.setItem("date", timeDateMonthInput.value);
+    localStorage.setItem("time", timeForTrainingInput.value);
+    localStorage.setItem("trainer", personalTrainerOfTheDayInput.value);
+
+    console.log(localStorage);
+}
+
+
+// sletter booking
+
+function deleteBooking () {
+    localStorage.removeItem("trainingType");
+    localStorage.removeItem("date");
+    localStorage.removeItem("time");
+    localStorage.removeItem("trainer");
+    alert("Booking has been deleted!")
+
+}
+
+
+
 
 
